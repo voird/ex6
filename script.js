1ecebf9b04a3cf5460ed75dc14c21538c116faab
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-18 22:44:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,63 +34,61 @@ window.addEventListener("DOMContentLoaded", function(event) {
 
     let s = document.getElementsByName("myselect");
     s[0].addEventListener("change", function(event) {
-      let select = event.target;
-      let radios = document.getElementById("myradios");
-      let check = document.getElementById("check-1");
-      if (select.value == "1") {
-        radios.style.display = "none";
-        check.style.display = "none";
-        let r = document.getElementById("result1");
-        var result = "2500";
-        r.innerHTML = result;
-      }
-      else if (select.value == "2") {
-        radios.style.display = "block";
-        check.style.display = "none";
-        let r = document.getElementById("result1");
-        var result = "Выберите из списка";
-        r.innerHTML = result;
-      }
-      else if(select.value == "3") {
-        radios.style.display = "none";
-        check.style.display = "block";
-        let r = document.getElementById("result1");
-        var result = "5000";
-        r.innerHTML = result;
-      }
+        let select = event.target;
+        let radios = document.getElementById("myradios");
+        let check = document.getElementById("check-1");
+        if (select.value == "1") {
+            radios.style.display = "none";
+            check.style.display = "none";
+            let r = document.getElementById("result1");
+            var result = "2500";
+            r.innerHTML = result;
+        } else if (select.value == "2") {
+            radios.style.display = "block";
+            check.style.display = "none";
+            let r = document.getElementById("result1");
+            var result = "Выберите из списка";
+            r.innerHTML = result;
+        } else if (select.value == "3") {
+            radios.style.display = "none";
+            check.style.display = "block";
+            let r = document.getElementById("result1");
+            var result = "5000";
+            r.innerHTML = result;
+        }
     });
 
     let r = document.querySelectorAll(".myradios input[type=radio]");
     r.forEach(function(radio) {
-      radio.addEventListener("change", function(event) {
-        let r = event.target;
-        if(r.value == "r1"){
-          let r = document.getElementById("result1");
-          var result = "34000";
-          r.innerHTML = result;
-        }else if (r.value == "r2") {
-          let r = document.getElementById("result1");
-          var result = "28000";
-          r.innerHTML = result;
-        }else if (r.value == "r3") {
-          let r = document.getElementById("result1");
-          var result = "50000";
-          r.innerHTML = result;
-        }
-      });
+        radio.addEventListener("change", function(event) {
+            let r = event.target;
+            if (r.value == "r1") {
+                let r = document.getElementById("result1");
+                var result = "34000";
+                r.innerHTML = result;
+            } else if (r.value == "r2") {
+                let r = document.getElementById("result1");
+                var result = "28000";
+                r.innerHTML = result;
+            } else if (r.value == "r3") {
+                let r = document.getElementById("result1");
+                var result = "50000";
+                r.innerHTML = result;
+            }
+        });
     });
 
     let c = document.getElementsByName("check-1");
     c[0].addEventListener("change", function(event) {
-      if(this.checked){
-        let r = document.getElementById("result1");
-        var result = "15000";
-        r.innerHTML = result;
-      }else{
-        let r = document.getElementById("result1");
-        var result = "10000";
-        r.innerHTML = result;
-      }
+        if (this.checked) {
+            let r = document.getElementById("result1");
+            var result = "15000";
+            r.innerHTML = result;
+        } else {
+            let r = document.getElementById("result1");
+            var result = "10000";
+            r.innerHTML = result;
+        }
     });
 
     let button = document.getElementById("ok");
